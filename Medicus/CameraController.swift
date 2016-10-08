4f7a26801a3232152a2c3622b138806dcaa9fb27
Jesse Bartola <jrbartola@gmail.com>
2016-10-08 19:45:25 -0400

    AVFoundation camera implementation

--- Medicus/CameraController.swift
+++ Medicus/CameraController.swift
@@ -16,14 +16,123 @@ class CameraController: UIViewController, UIImagePickerControllerDelegate, UINav
     @IBOutlet weak var cameraButton: UIButton!
     @IBOutlet weak var imageView: UIImageView!
     
+    let captureSession = AVCaptureSession()
+    var previewLayer : AVCaptureVideoPreviewLayer?
+    
+    // If we find a device we'll store it here for later use
+    var captureDevice : AVCaptureDevice?
+    
+    
+    
     let imagePicker = UIImagePickerController()
     
     override func viewDidLoad() {
         super.viewDidLoad()
 
         imagePicker.delegate = self
+        
+        captureSession.sessionPreset = AVCaptureSessionPresetHigh
+        
+        let devices = AVCaptureDeviceDiscoverySession(deviceTypes: [AVCaptureDeviceType.builtInTelephotoCamera,
+                                                                    AVCaptureDeviceType.builtInDuoCamera,
+                                                                    AVCaptureDeviceType.builtInWideAngleCamera], mediaType: nil,
+                                                      position: .back).devices
+        
+        // Loop through all the capture devices on this phone
+        for device in devices! {
+            // Make sure this particular device supports video
+            if (device.hasMediaType(AVMediaTypeVideo)) {
+                // Finally check the position and confirm we've got the back camera
+                if(device.position == AVCaptureDevicePosition.back) {
+                    captureDevice = device as? AVCaptureDevice
+                    if captureDevice != nil {
+                        print("Capture device found")
+                        beginSession()
+                    }
+                }
+            }
+        }
     }
-
+    
+    func focusTo(value: Float) {
+        if let device = captureDevice {
+            do {
+                try device.lockForConfiguration()
+                device.setFocusModeLockedWithLensPosition(value, completionHandler: { (time) -> Void in
+                    //
+                })
+                device.unlockForConfiguration()
+            } catch {
+                
+            }
+        }
+    }
+    
+    let screenWidth = UIScreen.main.bounds.size.width
+    
+    
+    func touchesBegan(touches: NSSet, withEvent event: UIEvent) {
+        var anyTouch = touches.anyObject() as! UITouch
+        var touchPercent = anyTouch.location(in: self.view).x / screenWidth
+        focusTo(value: Float(touchPercent))
+    }
+    
+    func touchesMoved(touches: NSSet, withEvent event: UIEvent) {
+        
+        var anyTouch = touches.anyObject() as! UITouch
+        var touchPercent = anyTouch.location(in: self.view).x / screenWidth
+        focusTo(value: Float(touchPercent))
+    }
+    
+    func configureDevice() {
+        if let device = captureDevice {
+            do {
+                try device.lockForConfiguration()
+                device.focusMode = .locked
+                print("Devce is being configured.")
+                //device.isAdjustingFocus = true
+                
+                device.unlockForConfiguration()
+            } catch {
+                // Else
+                print("error")
+            }
+            
+        }
+        
+    }
+    
+    func beginSession() {
+        
+        configureDevice()
+        print("beginning")
+        var err : NSError? = nil
+        do {
+            do {
+                let input = try AVCaptureDeviceInput(device: captureDevice)
+                try captureSession.addInput(input)
+            } catch {
+                print("error")
+            }
+            
+        } catch let error as NSError {
+            print(error)
+        }
+        
+        
+        
+        if err != nil {
+            print("error: \(err?.localizedDescription)")
+        }
+        
+        previewLayer = AVCaptureVideoPreviewLayer(session: captureSession)
+        self.view.layer.addSublayer(previewLayer!)
+        previewLayer?.frame = self.view.layer.frame
+        print(self.view.layer.frame)
+        captureSession.startRunning()
+    }
+    
+    
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
@@ -31,11 +140,12 @@ class CameraController: UIViewController, UIImagePickerControllerDelegate, UINav
     
     // MARK: - Delegates
     func imagePickerController(_ picker: UIImagePickerController, didFinishPickingMediaWithInfo info: [String : Any]) {
+        print("Does this get executedd?")
         var chosenImage = UIImage()
         chosenImage = info[UIImagePickerControllerOriginalImage] as! UIImage
         imageView.contentMode = .scaleAspectFit
         imageView.image = chosenImage
-        //imageView.image
+
         print("Image is \(chosenImage)")
         dismiss(animated:true, completion: nil)
     }
@@ -45,17 +155,14 @@ class CameraController: UIViewController, UIImagePickerControllerDelegate, UINav
     }
     
     @IBAction func openCameraButton(_ sender: UIButton) {
-        print("Camera is activating")
-        if UIImagePickerController.isSourceTypeAvailable(.camera) {
+        /*if UIImagePickerController.isSourceTypeAvailable(.camera) {
             
             let imagePicker = UIImagePickerController()
 
             imagePicker.sourceType = .camera
             imagePicker.modalPresentationStyle = .fullScreen
-            self.present(imagePicker, animated: true, completion: { () in
-                print("Camera is open")
-            })
-        }
+            self.present(imagePicker, animated: true, completion: nil)
+        }*/
         
     }
     
