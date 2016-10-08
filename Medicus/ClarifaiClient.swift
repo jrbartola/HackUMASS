6b3fde5ca0bae9c7cfdfed21e3c88393c401e1bf
Jesse Bartola <jrbartola@gmail.com>
2016-10-08 04:30:45 -0400

    getConcepts function implementation

--- Medicus/ClarifaiClient.swift
+++ Medicus/ClarifaiClient.swift
@@ -11,12 +11,45 @@ import Clarifai
 
 class ClarifaiClient {
     
-    var apiKey: String
-    var secretKey: String
+    let app: ClarifaiApp
+    var picture: ClarifaiImage?
     
     init(apiKey key: String, secretKey secret: String) {
-        apiKey = key
-        secretKey = secret
+        app = ClarifaiApp(appID: key, appSecret: secret)
     }
+    
+    func getConcepts(url: String, modelName: String, conceptCompletion: @escaping (([String]?) -> Void)) {
+        let image = ClarifaiImage.init(url: url)
+        app.getModelByName("general-v1.3", completion: { (model: ClarifaiModel?, error: Error?) in
+            
+            model?.predict(on: [image!], completion: { (output, error) in
+                if let concepts = output?[0].concepts {
+                    var arr: [String] = []
+                    // Iterate through concepts and add them to an array as Strings
+                    for concept in concepts {
+                        arr.append(concept.conceptName!)
+                    }
+                    conceptCompletion(arr)
+                } else {
+                    conceptCompletion(nil)
+                }
+                
+            })
+            
+        })
+    }
+    
+    // Default modelName is 'general-v1.3'
+    func getConcepts(url: String, conceptCompletion: @escaping (([String]?) -> Void)) {
+        return getConcepts(url: url, modelName: "general-v1.3", conceptCompletion: { (concepts) in
+            if let cps = concepts {
+                conceptCompletion(cps)
+            } else {
+                conceptCompletion(nil)
+            }
+        })
+        
+    }
+    
     
 }
