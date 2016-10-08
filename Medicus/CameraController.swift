fe3744bffd893bd1f914e8349a22b97b8aaf118a
Jesse Bartola <jrbartola@gmail.com>
2016-10-08 17:16:04 -0400

    Camera view controller

---
+++ Medicus/CameraController.swift
@@ -0,0 +1,26 @@
+//
+//  CameraController.swift
+//  Medicus
+//
+//  Created by Jesse Bartola on 10/8/16.
+//  Copyright © 2016 VJRE. All rights reserved.
+//
+
+import UIKit
+import MobileCoreServices
+
+class CameraController: UIViewController, UIImagePickerControllerDelegate, UINavigationControllerDelegate {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+
+}
